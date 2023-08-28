8b1affe3a9ead0b4a2fff90688861b1a466bf21c
biduedson <112134265+biduedson@users.noreply.github.com>
2023-08-28 12:29:45 -0300

    feito exercicio2

--- index.ts
+++ index.ts
@@ -1,11 +1,19 @@
 const fs = require("fs");
 
-const lerArquivo = (caminho: string): unknown => {
-  return JSON.parse(fs.readFileSync(caminho));
+const lerArquivo = (): unknown => {
+  return JSON.parse(fs.readFileSync("./bd.json"));
 };
 
-const escreverArquivo = (dados: any, caminho: string): void => {
-  fs.writeFileSync(caminho, JSON.stringify(dados));
+const escreverArquivo = (dados: any): void => {
+  fs.writeFileSync("./bd.json", JSON.stringify(dados));
+};
+
+type Endereco = {
+  cep: string;
+  rua: string;
+  complemento: string;
+  bairro: string;
+  cidade: string;
 };
 
 type Usuario = {
@@ -13,11 +21,48 @@ type Usuario = {
   email: string;
   cpf: string;
   profissao: string;
+  endereco: Endereco;
 };
 
 let usuario: Usuario = {
   nome: "Edson Gomes",
   email: "bidu@bidu.com",
   cpf: "12345678977",
-  profissao: "Programador"
+  profissao: "Programador",
+  endereco: {
+    cep: "07918120",
+    rua: "aldo lupo",
+    complemento: "casa 02",
+    bairro: "aparecida",
+    cidade: "morato"
+  }
 };
+
+const cadastroUsuario = (usuario: Usuario): Usuario => {
+  const bd = lerArquivo() as Usuario[];
+  bd.push(usuario);
+  escreverArquivo(bd);
+  return usuario;
+};
+
+const listarUsuarios = (): Usuario[] => {
+  return lerArquivo() as Usuario[];
+};
+
+const edson = cadastroUsuario({
+  nome: "Antonio Carlos",
+  email: "bidu@bidu.com",
+  cpf: "12345678977",
+  profissao: "Programador",
+  endereco: {
+    cep: "07918120",
+    rua: "aldo lupo",
+    complemento: "casa 02",
+    bairro: "aparecida",
+    cidade: "morato"
+  }
+});
+
+const bd = lerArquivo();
+
+console.log(edson, bd);
