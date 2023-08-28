e09e6881603ec32050fde0fd5dae88580b757bfc
biduedson <112134265+biduedson@users.noreply.github.com>
2023-08-28 12:54:38 -0300

    feito exercicio 3

--- index.ts
+++ index.ts
@@ -49,7 +49,31 @@ const listarUsuarios = (): Usuario[] => {
   return lerArquivo() as Usuario[];
 };
 
-const edson = cadastroUsuario({
+const detalharUsuario = (cpf: string): Usuario => {
+  const bd = lerArquivo() as Usuario[];
+  const usuario = bd.find((usuario) => {
+    return usuario.cpf === cpf;
+  });
+  if (!usuario) {
+    throw new Error("Usuario não encontrado");
+  }
+  return usuario;
+};
+
+const atualizarUsuario = (cpf: string, dados: Usuario): void => {
+  const bd = lerArquivo() as Usuario[];
+  const usuario = bd.find((usuario) => {
+    return usuario.cpf === cpf;
+  });
+  if (!usuario) {
+    throw new Error("Usuario não encontrado");
+  }
+
+  Object.assign(usuario, dados); // Usado para copiar valores de todas as propriedades de um objeto pelo outro
+  console.log(usuario);
+};
+
+/*const edson = cadastroUsuario({
   nome: "Antonio Carlos",
   email: "bidu@bidu.com",
   cpf: "12345678977",
@@ -61,8 +85,21 @@ const edson = cadastroUsuario({
     bairro: "aparecida",
     cidade: "morato"
   }
-});
-
+});*/
+const edson = detalharUsuario("12345678977");
 const bd = lerArquivo();
-
-console.log(edson, bd);
+atualizarUsuario("12345678977", {
+  nome: "Edson Gomes",
+  email: "bidu@bidu.com",
+  cpf: "12345678977",
+  profissao: "Programador",
+  endereco: {
+    cep: "07918120",
+    rua: "aldo lupo",
+    complemento: "casa 02",
+    bairro: "aparecida",
+    cidade: "morato"
+  }
+});
+//console.log(edson);
+//console.log(edson, bd);
